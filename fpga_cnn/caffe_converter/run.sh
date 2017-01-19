bf73623d0894a608aca2bf8b4be5ad7169ac2965
Yao Chen <yaochen_nk@hotmail.com>
2017-01-19 14:08:51 +0800

    Reorganized folder structure. Rename caffe converter file.

--- fpga_cnn/caffe_converter/run.sh
+++ fpga_cnn/caffe_converter/run.sh
@@ -1,5 +1,5 @@
-g++ -O3 examples/caffe_converter/caffe_converter_1.cpp  tiny_dnn/io/caffe/caffe.pb.cc -o caffe_converter_1 -lprotobuf -pthread -Wall -Wextra -pedantic -I ./ `pkg-config --cflags --libs opencv`
-
-
-./caffe_converter_1 ./lenet/lenet_5_deploy.prototxt ./lenet/lenet_5_iter_10000.caffemodel 
+#compile caffe converter
+g++ -O3 caffe_converter.cpp  caffe.pb.cc -o caffe_converter -lprotobuf -pthread -Wall -Wextra -pedantic -I ./ `pkg-config --cflags --libs opencv`
+#run converter to converte targeted caffe model and weight files
+./caffe_converter ../../example/demo/lenet/lenet_5_deploy.prototxt ../../example/demo/lenet/lenet_5_iter_10000.caffemodel 
 
