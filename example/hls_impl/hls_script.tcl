a85c5fe42853cb342c9e342aa18aeec90b8e9167
Yao Chen <yaochen_nk@hotmail.com>
2017-02-23 16:46:24 +0800

    Updated hls script

--- example/hls_impl/hls_script.tcl
+++ example/hls_impl/hls_script.tcl
@@ -2,34 +2,34 @@
 ## Please DO NOT edit it.
 ## Copyright (C) 2015 Xilinx Inc. All rights reserved.
 ############################################################
-open_project hls_proj_c2m2_cpfuse_convbuf
+open_project hls_proj_lenet
 
 #set_top convolution_layer_with_table
 #set_top pooling_layer
 set_top inference_net
 
-add_files ../fpga_cnn/activation_functions.h
-add_files ../fpga_cnn/pool_layer.h
-add_files ../fpga_cnn/config.h
-add_files ../fpga_cnn/construct_net.h
-add_files ../fpga_cnn/conv_layer.h
-add_files ../fpga_cnn/data_type.h
-add_files ../fpga_cnn/fc_layer.h
+add_files ../../fpga_cnn/activation_functions.h
+add_files ../../fpga_cnn/pool_layer.h
+add_files ../../fpga_cnn/config.h
+add_files ../../fpga_cnn/construct_net.h
+add_files ../../fpga_cnn/conv_layer.h
+add_files ../../fpga_cnn/data_type.h
+add_files ../../fpga_cnn/fc_layer.h
 
-add_files ../fpga_cnn/hls_lib/static_vector.h
+add_files ../../fpga_cnn/hls_lib/static_vector.h
 
-add_files -tb ../fpga_cnn/image_converter.h
-add_files -tb ../fpga_cnn/weight_bias.h
-add_files -tb ../stb_image/stb_image.h
-add_files -tb ../stb_image/stb_image_resize.h
-add_files -tb ../stb_image/stb_image_write.h
+add_files -tb ../../fpga_cnn/image_converter.h
+add_files -tb ../../fpga_cnn/weight_bias.h
+add_files -tb ../../stb_image/stb_image.h
+add_files -tb ../../stb_image/stb_image_resize.h
+add_files -tb ../../stb_image/stb_image_write.h
 
 
-add_files ff_test.cpp
+add_files ../ff_test.cpp
 #add_files -tb 4.bmp
-add_files -tb input.txt
+add_files -tb ../input.txt
 #add_files -tb LeNet-weights
-add_files -tb weights_batch.txt
+add_files -tb ../weights_batch.txt
 
 
 add_files -cflags "-std=c++0x -fpermissive -pedantic -Wall -Wextra" -tb ff_test.cpp
