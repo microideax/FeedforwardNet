25518fdd2e4782a062e38534c29673c5f110d132
yuyang <455236108@qq.com>
2017-02-26 10:19:38 +0800

    changed to the conv+pooling version & changed weight_bias.h

--- fpga_cnn/construct_net.h
+++ fpga_cnn/construct_net.h
@@ -27,7 +27,7 @@ void inference_net(
 	char activation_type,
 
 	// input pic data
-	float in_data_3D[1][32][32],
+	float in_data_3D[1][28][28],
 
 
 	// layer weights and bias inputs ------- conv2pool2fc
@@ -122,10 +122,12 @@ char relu = 'r';
 	/******************************************************************************************/
 
 	//construct network --------------conv(1-6) + max Pooling + conv(6-16) + max pooling + fc
-	conv_layer<float, 32, 5, 1, 6> C1;
-	pool_layer<float, 28, 2, 6> P2;
-	conv_layer<float, 14, 5, 6, 16> C3;     //6@14x14-in, 16@10x10-out
-	pool_layer<float, 10, 2, 16> P4;
+    conv_pool_layer<float, 28, 5, 2, 1, 2, 0, 2, 1, 6> C1P2;
+	//conv_layer<float, 32, 5, 1, 6> C1;
+	//pool_layer<float, 28, 2, 6> P2;
+	conv_pool_layer<float, 14, 5, 0, 1, 2, 0, 2, 6, 16> C3P4;
+	//conv_layer<float, 14, 5, 6, 16> C3;     //6@14x14-in, 16@10x10-out
+	//pool_layer<float, 10, 2, 16> P4;
 	fc_layer<float, 16, 5, 10> F5;
 
 	//temp storage space
@@ -135,12 +137,12 @@ char relu = 'r';
 	float  pool_2_out[16][5][5] = { 0 };
 
 	//Forward propagation process
-	C1.conv_layer_a(activation_type, in_data_3D, conv_1_weight_a, conv_1_bias_a, conv_1_out);
-//    C1.conv_layer_w_pool_a(activation_type, in_data_3D, conv_1_weight_a, conv_1_bias_a, pool_1_out);
-//    C3.conv_layer_w_pool_a(activation_type, pool_1_out, conv_2_weight_a, conv_2_bias_a, pool_2_out);
-	P2.max_pooling_layer_a(activation_type, conv_1_out, pool_1_out);
-	C3.conv_layer_a(activation_type, pool_1_out, conv_2_weight_a, conv_2_bias_a, conv_2_out);
-	P4.max_pooling_layer_a(activation_type, conv_2_out, pool_2_out);
+//	C1.conv_layer_a(activation_type, in_data_3D, conv_1_weight_a, conv_1_bias_a, conv_1_out);
+	C1P2.conv_layer_w_pool_a(activation_type, in_data_3D, conv_1_weight_a, conv_1_bias_a, pool_1_out);
+	C3P4.conv_layer_w_pool_a(activation_type, pool_1_out, conv_2_weight_a, conv_2_bias_a, pool_2_out);
+//	P2.max_pooling_layer_a(activation_type, conv_1_out, pool_1_out);
+//	C3.conv_layer_a(activation_type, pool_1_out, conv_2_weight_a, conv_2_bias_a, conv_2_out);
+//	P4.max_pooling_layer_a(activation_type, conv_2_out, pool_2_out);
 	F5.fc_layer_a(activation_type, pool_2_out, fc_1_weight_a, fc_1_bias_a, fc_1_out_a);
 //    F5.fc_layer_a(activation_type, pool_2_out, fc_1_weight_a, fc_1_bias_a, fc_1_out_a);
 	/******************************************************************************************/
