3dcd6f7cdbf64f87f721c2c255d6c4b145b54b9d
Yao Chen <yaochen_nk@hotmail.com>
2017-03-16 15:09:33 +0800

    Modified hls_script.

--- example/hls_impl/hls_script.tcl
+++ example/hls_impl/hls_script.tcl
@@ -2,7 +2,7 @@
 ## Please DO NOT edit it.
 ## Copyright (C) 2015 Xilinx Inc. All rights reserved.
 ############################################################
-open_project hls_proj_lenet
+open_project hls_proj_alexnet
 
 #set_top convolution_layer_with_table
 #set_top pooling_layer
@@ -16,6 +16,8 @@ add_files ../../fpga_cnn/construct_net.h
 add_files ../../fpga_cnn/conv_layer.h
 add_files ../../fpga_cnn/data_type.h
 add_files ../../fpga_cnn/fc_layer.h
+add_files ../../fpga_cnn/lrn_layer.h
+add_files ../../fpga_cnn/dropout_layer.h
 
 add_files -tb ../../fpga_cnn/accuracy.h
 #add_files -tb ../../read_mnist.h
@@ -28,23 +30,25 @@ add_files -tb ../../stb_image/stb_image_resize.h
 add_files -tb ../../stb_image/stb_image_write.h
 
 add_files ../ff_test.cpp
-add_files -tb ../input_3.txt
-add_files -tb ../weights_bias_relu.txt
+add_files -tb ../alexnet_input.txt
+add_files -tb ../weights_alexnet.txt
 
 
 add_files -cflags "-std=c++0x -fpermissive -pedantic -Wall -Wextra" -tb ../ff_test.cpp
 
 
 open_solution -reset "fpga_cnn"
+#vc709
+set_part {xc7vx690tffg1761-2}
 #zynq-7000 board
 #set_part {xc7z045ffg900-1} 
 # zedboard
-set_part {xc7z020clg484-1}  
+#set_part {xc7z020clg484-1}  
 create_clock -period 10 -name default
 
 
-csim_design -clean -argv {weights_bias_relu, input_3.txt}
+csim_design -clean -argv {weights_alexnet, alexnet_input.txt}
 
 csynth_design
 
-cosim_design -argv {weights_bias_relu, input_3.txt} -trace_level none -rtl verilog -tool xsim
+cosim_design -argv {weights_alexnet, alexnet_input.txt} -trace_level none -rtl verilog -tool xsim
