f2b10a68d8396665204edbde5c460ee7d6df3429
yuyang <455236108@qq.com>
2017-04-03 20:26:18 +0800

    minor change on lenet_demo_1

--- example/lenet_demo_1/ff_test.cpp
+++ example/lenet_demo_1/ff_test.cpp
@@ -17,7 +17,6 @@
 #include "../../fpga_cnn/image_converter.h"
 #include "../../fpga_cnn/weight_bias.h"
 #include "../../fpga_cnn/conv_layer.h"
-#include "../../fpga_cnn/pool_layer.h"
 #include "../../fpga_cnn/data_quantize.h"
 #include "../../fpga_cnn/read_mnist.h"
 #include "../../fpga_cnn/softmax.h"
