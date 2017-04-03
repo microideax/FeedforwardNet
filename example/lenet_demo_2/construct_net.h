d1b8cf5a66c8285c15ba383b15779880b3dca476
yuyang <455236108@qq.com>
2017-04-03 16:43:41 +0800

    mirror change on construct_net.h

--- example/lenet_demo_2/construct_net.h
+++ example/lenet_demo_2/construct_net.h
@@ -58,15 +58,9 @@ void inference_net(
 	/*****************************************************************************************/
 	//construct network --------------LeNet-5
 	conv_layer<float, 32, 5, 0, 1, 1, 6, 1> C1;      //1@32x32-in, 6@28x28-out
-<<<<<<< HEAD
 	pool_layer<float, 28, 2, 0, 2, 6> P2;         //6@28x28-in, 6@14x14-out
 	conv_layer<float, 14, 5, 0, 1, 6, 16, 1> C3;     //6@14x14-in, 16@10x10-out
 	pool_layer<float, 10, 2, 0, 2, 16> P4;        //16@10x10-in, 16@5x5-out
-=======
-	pool_layer<float, 28, 2, 0,2, 6> P2;         //6@28x28-in, 6@14x14-out
-	conv_layer<float, 14, 5, 0, 1, 6, 16, 1> C3;     //6@14x14-in, 16@10x10-out
-	pool_layer<float, 10, 2, 0,2, 16> P4;        //16@10x10-in, 16@5x5-out
->>>>>>> FeedforwardNet/master
 	conv_layer<float, 5, 5, 0, 1, 16, 120, 1> C5;    //16@5x5-in, 120@1x1-out
 	fc_layer<float, 120, 1, 10> F6;         //120@1x1-in, 10@1x1-out
 
@@ -80,7 +74,7 @@ void inference_net(
 	//Forward propagation process
 	C1.conv_layer_a(activation_type, in_data_3D, conv_1_weight_a, conv_1_bias_a, conv_1_out_a);
 	P2.pooling_layer_a(activation_type, conv_1_out_a, pool_1_weight_a, pool_1_bias_a, pool_1_out_a);
-	C3.conv_layer_a(activation_type, pool_1_out_a, conv_2_weight_a, conv_2_bias_a, conv_2_out_a);
+	C3.conv_layer_a_table(activation_type, has_connection_table[1], pool_1_out_a, conv_2_weight_a, conv_2_bias_a, conv_2_out_a);
 	P4.pooling_layer_a(activation_type, conv_2_out_a, pool_2_weight_a, pool_2_bias_a, pool_2_out_a);
 	C5.conv_layer_a(activation_type, pool_2_out_a, conv_3_weight_a, conv_3_bias_a, conv_3_out_a);
 	F6.fc_layer_a(activation_type, conv_3_out_a, fc_1_weight_a, fc_1_bias_a, fc_1_out_a);
