acafb9dc5a87582253f6ceaee0b732ad19bc9d1a
Yao Chen <yaochen_nk@hotmail.com>
2017-09-20 20:21:04 +0800

    Temp note.

--- caffe_converter/run.sh
+++ caffe_converter/run.sh
@@ -1,9 +1,9 @@
 
 
 #compile caffe converter
-g++ -std=c++11 -O3 caffe_converter.cpp  caffe.pb.cc -o caffe_converter -lprotobuf -pthread -Wall -Wextra -pedantic -I ./ `pkg-config --cflags --libs opencv`
+#g++ -std=c++11 -O3 caffe_converter.cpp  caffe.pb.cc -o caffe_converter -lprotobuf -pthread -Wall -Wextra -pedantic -I ./ `pkg-config --cflags --libs opencv`
 
 #run converter to converte targeted caffe model and weight files
-#./caffe_converter ../../example/caffe_demos/lenet/lenet_2conv2max/lenet_2conv2max_deploy.prototxt ../../example/caffe_demos/lenet/lenet_2conv2max/lenet_2conv2max_iter_10000.caffemodel
+./caffe_converter ../example/caffe_demos/lenet/lenet_2conv2max_relu/lenet_2conv2max_relu_deploy.prototxt ../example/caffe_demos/lenet/lenet_2conv2max_relu/lenet_2conv2max_relu_iter_10000.caffemodel
 
-#./caffe_converter ../../example/caffe_demos/alexnet/deploy.prototxt ../../example/caffe_demos/alexnet/bvlc_alexnet.caffemodel ../../example/caffe_demos/alexnet/imagenet_mean.binaryproto 
+#./caffe_converter ../example/caffe_demos/vgg16_demo/VGG_ILSVRC_16_layers_deploy.prototxt ../example/caffe_demos/vgg16_demo/VGG_ILSVRC_16_layers.caffemodel ../example/caffe_demos/vgg16_demo/imagenet_mean.binaryproto 
