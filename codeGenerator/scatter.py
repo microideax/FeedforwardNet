f773f2b1e89cfbfd671f9ab5183e4d46637db64a
Yao Chen <yaochen_nk@hotmail.com>
2018-01-01 17:03:26 +0800

    Temp update.

--- codeGenerator/scatter.py
+++ codeGenerator/scatter.py
@@ -4,21 +4,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # generate data
+# Tr=Tc=64
 x1 = [32, 64, 96, 128]
-x2 = []
+x2 = [8, 16, 32, 64]
 x3 = []
 x4 = []
 k = []
-x = [float(xi * 8 * 16 * 16 * 25) for xi in x1]
+x = []
+#x = [float(xi * 8 * 32 * 32 * 25) for xi in x1]
+for tm in x1:
+    for tn in x2:
+        x.append(tm*tn*32*32*25)
 
 Y = []
 X = []
 
+#for xi in x:
+#    for si in xrange(1, 11):
+#        X.append(xi)
+        #yi = ((xi * 8 * 25 + 8 * (15 * si + 5) ** 2 + xi * 16 * 16) * 4) / ((16 * 16 * 25 + xi*16 * 16) * 10) / 100000 * si
+#        yi = ((xi * 8 * 25 + 8 * (54 * si + 5) ** 2 + xi * 55 * 55) * 4) / ((55 * 55 * 25 + (xi/4) * 55 * 55) * 10)
+#        #yi = ((xi * 8 * 25 + 8 * (15 * si + 5) ** 2 + xi * 16 * 16) * 4) / ((16 * 16 * 25 + xi*16 * 16) * 10)
+#        Y.append(yi)
 for xi in x:
-    for si in xrange(1, 6):
+    for si in xrange(1, 11):
         X.append(xi)
-        # yi = ((xi * 8 * 25 + 8 * (15 * si + 5) ** 2 + xi * 16 * 16) * 4) / ((16 * 16 * 25 + 16 * 16) * 10) / 100000 * si
-        yi = ((xi * 8 * 25 + 8 * (15 * si + 5) ** 2 + xi * 16 * 16) * 4) / ((16 * 16 * 25 + xi*16 * 16) * 10)
+        #yi = ((xi * 8 * 25 + 8 * (15 * si + 5) ** 2 + xi * 16 * 16) * 4) / ((16 * 16 * 25 + xi*16 * 16) * 10) / 100000 * si
+        yi = ((xi * 8 * 25 + 8 * (54 * si + 5) ** 2 + xi * 55 * 55) * 4) / ((55 * 55 * 25 + (xi/4) * 55 * 55) * 10)
+        #yi = ((xi * 8 * 25 + 8 * (15 * si + 5) ** 2 + xi * 16 * 16) * 4) / ((16 * 16 * 25 + xi*16 * 16) * 10)
         Y.append(yi)
 
 print X
