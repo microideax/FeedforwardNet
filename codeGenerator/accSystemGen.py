48c3b97d6a6a9195109680a40c49ee60970db4a6
Yao Chen <yao@Yaos-MacBook-Pro.local>
2018-06-08 00:13:38 +0800

    Initial system gen script.

---
+++ codeGenerator/accSystemGen.py
@@ -0,0 +1,16 @@
+import sys
+
+
+#param format <dtype, dtype, dtype, Tm, Tn, Tr, Tc>
+def genConvAccParam():
+    param.appdix()
+
+def createConvAcc(number):
+
+    temp_text = "convAcc< > convAcc" + number + "\n"
+    temp_text = "conv_layer_acc() " + "\n"
+
+    return temp_text
+
+def createPoolAcc():
+    temp_text = " "
